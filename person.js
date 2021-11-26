03286bb42655238bf36bc9f3b2e644c24f620015
SHIVAM JAISWAL <90027183+ShivamJaiswal2006101@users.noreply.github.com>
2021-11-26 21:58:49 +0530

    Create person.js

---
+++ person.js
@@ -0,0 +1,29 @@
+var Person1 = new Object()
+Person1.FirstName = "Shivam",
+    Person1.LastName = "Jaiswal",
+    Person1.OfficeAddress = "Pattigar T. Bahadurganj"
+
+var Person2 = new Object()
+Person2.FirstName = "Pawan",
+    Person2.LastName = "Jaiswal",
+    Person2.OfficeAddress = "Ghazipur,Uttar Pradesh"
+
+var Person3 = new Object()
+Person3.FirstName = "Amitesh",
+    Person3.LastName = "Kumar",
+    Person3.OfficeAddress = "Patna,Bihar"
+
+console.log("Person1:");
+console.log("FirstName: " + Person1.FirstName);
+console.log("LastName: " + Person1.LastName);
+console.log("OfficeAddress: " + Person1.OfficeAddress);
+
+console.log("Person2:");
+console.log("FirstName: " + Person2.FirstName);
+console.log("LastName: " + Person2.LastName);
+console.log("OfficeAddress: " + Person2.OfficeAddress);
+
+console.log("Person3:");
+console.log("FirstName: " + Person3.FirstName);
+console.log("LastName: " + Person3.LastName);
+console.log("OfficeAddress: " + Person3.OfficeAddress);
